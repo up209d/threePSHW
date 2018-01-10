fa3337ecf66a8ab56acf71c1af311faa911b45fc
up209d <up209d@gmail.com>
2018-01-10 17:14:40 +1100

    Update environment

--- app/scripts/pages/home.js
+++ app/scripts/pages/home.js
@@ -65,10 +65,201 @@ function dlHomeController($scope,$timeout,$element) {
     self.scene =  new THREE.Scene();
     self.controls = new THREE.OrbitControls(self.camera);
 
+    // Lights
+    self.lights = [];
+
+    self.lights[0] = new THREE.DirectionalLight('#00FF00', 1);
+    self.lights[0].position.set(-1,1,1);
+    self.scene.add(self.lights[0]);
+
+    self.lights[1] = new THREE.DirectionalLight('#ff0000', 1);
+    self.lights[1].position.set(1,1,1);
+    self.scene.add(self.lights[1]);
+
+    self.lights[2] = new THREE.PointLight('#0000ff', 1);
+    self.lights[2].position.set(0,0,0);
+    self.scene.add(self.lights[2]);
 
     // Meshes
 
+    let createCubes = function(options) {
+      if (typeof options === 'undefined') {
+        options = {}
+      }
 
+      options = Object.assign({
+        gridSize: 100,
+        gridLength: 30,
+        gridPadding: 1,
+        delta: new THREE.Vector3(0,600,0),
+        duration: 15.0,
+        totalDelay: 5.0
+      },options);
+
+      let cubeSize = options.gridSize / options.gridLength;
+      let cubeCount = Math.pow(options.gridLength,3);
+
+      let prefab = new THREE.PlaneGeometry(
+        cubeSize-options.gridPadding,
+        cubeSize-options.gridPadding,
+        1//cubeSize-options.gridPadding
+      );
+
+      let tempGeometry = new THREE.BoxBufferGeometry(
+        cubeSize-options.gridPadding,
+        cubeSize-options.gridPadding,
+        cubeSize-options.gridPadding
+      );
+
+      let geometry = new THREE.BAS.PrefabBufferGeometry(prefab,cubeCount);
+
+      console.log(tempGeometry);
+      console.log(geometry);
+
+      let startPositionBuffer = geometry.createAttribute('startPosition',3);
+      let endPositionBuffer = geometry.createAttribute('endPosition',3);
+      let rotationBuffer = geometry.createAttribute('rotation',4);
+      let durationBuffer = geometry.createAttribute('duration',1);
+      let startTimeBuffer = geometry.createAttribute('startTime',1);
+      let cubeIndexBuffer = geometry.createAttribute('cubeIndex',1);
+
+      let cubeIndex = 0;
+
+      // Just save memory by re-use this memory block
+      // instead of create a new memory address by local variable
+      let tmpa = [];
+
+      for (let x = 0; x < options.gridLength ; x++) {
+        for (let y = 0; y < options.gridLength ; y++) {
+          for (let z = 0; z < options.gridLength ; z++) {
+
+            // Start Position
+            tmpa[0] = THREE.Math.mapLinear(x,0,options.gridLength-1,-options.gridSize/2,options.gridSize/2) + options.delta.x;
+            tmpa[1] = THREE.Math.mapLinear(y,0,options.gridLength-1,-options.gridSize/2,options.gridSize/2) + options.delta.y;
+            tmpa[2] = THREE.Math.mapLinear(z,0,options.gridLength-1,-options.gridSize/2,options.gridSize/2) + options.delta.z;
+            geometry.setPrefabData(startPositionBuffer, cubeIndex, [tmpa[0],tmpa[1],tmpa[2]]);
+
+            // End Position
+            tmpa[0] = THREE.Math.mapLinear(x,0,options.gridLength-1,-options.gridSize/2,options.gridSize/2) - options.delta.x;
+            tmpa[1] = THREE.Math.mapLinear(y,0,options.gridLength-1,-options.gridSize/2,options.gridSize/2) - options.delta.y;
+            tmpa[2] = THREE.Math.mapLinear(z,0,options.gridLength-1,-options.gridSize/2,options.gridSize/2) - options.delta.z;
+            geometry.setPrefabData(endPositionBuffer, cubeIndex, [tmpa[0],tmpa[1],tmpa[2]]);
+
+            // Rotation
+            tmpa[0] = Math.random()*2-1;
+            tmpa[1] = Math.random()*2-1;
+            tmpa[2] = Math.random()*2-1;
+            tmpa[3] = Math.PI * 2;
+            geometry.setPrefabData(rotationBuffer, cubeIndex, [tmpa[0],tmpa[1],tmpa[2],tmpa[3]]);
+
+
+
+            tmpa[4] = options.duration;
+            geometry.setPrefabData(durationBuffer, cubeIndex, [tmpa[4]]);
+
+            tmpa[5] = (options.totalDelay / cubeCount) * cubeIndex * Math.random() * options.totalDelay;
+            geometry.setPrefabData(startTimeBuffer, cubeIndex, [tmpa[5]]);
+
+            tmpa[6] = cubeIndex;
+            geometry.setPrefabData(cubeIndexBuffer, cubeIndex, [tmpa[6]]);
+
+            cubeIndex++;
+          }
+        }
+      }
+
+      let material = new THREE.BAS.PhongAnimationMaterial({
+        flatShading: THREE.FlatShading,
+        wireframe: true,
+        uniforms: {
+          time: {
+            value: 0
+          }
+        },
+        // DONT FORGET SEMI-COLON
+        // Assign values, assign functions here
+        vertexFunctions: [
+          `
+            float rand(float n){return fract(sin(n) * 43758.5453123);}
+            
+            float noise(float p){
+              float fl = floor(p);
+              float fc = fract(p);
+              return mix(rand(fl), rand(fl + 1.0), fc);
+            }
+            
+            vec3 cubicBezier(vec3 p0, vec3 c0, vec3 p1, vec3 c1, float t) {   
+              float tn = 1.0 - t;
+              return 
+                tn * tn * tn * p0 + 
+                3.0 * tn * tn * t * c0 + 
+                3.0 * tn * t * t * c1 + 
+                t * t * t * p1;
+            }
+            
+            vec3 sample(vec3 startPosition,vec3 endPosition,float progress, float radius) {
+              vec3 position;
+              float angle = progress * PI/2.0;
+              float randomFactor = (1.0-rand(angle+cubeIndex))*sin(angle)*cos(angle)*radius*100.0;
+              position.x = startPosition.x + radius*sin(angle*20.0);
+              position.y = startPosition.y*cos(angle) + endPosition.y*sin(angle);
+              position.z = startPosition.z + radius*sin(angle*24.0);
+              return position;
+            }
+            
+          `,
+          THREE.BAS.ShaderChunk['ease_sine_in_out'],
+          THREE.BAS.ShaderChunk['quaternion_rotation']
+        ],
+        vertexParameters: [
+          'uniform float time;',
+          'attribute vec3 startPosition;',
+          'attribute vec3 endPosition;',
+          'attribute vec4 rotation;',
+          'attribute float startTime;',
+          'attribute float duration;',
+          'attribute float cubeIndex;'
+        ],
+        vertexPosition: [
+          'float random = rand(time);',
+          'float progress = (clamp(time - startTime, 0.0, duration) / duration);',
+          'float noiseFactor = noise(cubeIndex+time)/2.0;',
+
+          // Easing for progress
+          'progress = easeSineInOut(progress);',
+
+          // Scale should be calculated before positioning
+          'float scaleFactor = 1.0 + (sin(progress * PI)*noiseFactor);',
+          'transformed *= scaleFactor;',
+
+          // Rotation should be calculated before positioning
+          'vec4 quaternion = quatFromAxisAngle(rotation.xyz,rotation.w * progress + rotation.w*noiseFactor);',
+          'transformed = rotateVector(quaternion, transformed);',
+
+          // Positioning
+          // 'vec3 _endPosition = endPosition + vec3(0,0,0);',
+          // Linear Positioning
+          // 'transformed += mix(startPosition,endPosition,progress);'
+          // Cubic Bezier Positioning
+          // 'transformed += cubicBezier(startPosition,startPosition+vec3(0,200,0),endPosition,endPosition+vec3(0,-200,0),progress);'
+          // Custom Function Positioning
+          'transformed += sample(startPosition,endPosition,progress,cubeIndex/10.0);',
+        ],
+        fragmentParameters: [
+
+        ],
+        fragmentSpecular: [
+
+        ]
+      });
+
+      self.cubes = new THREE.Mesh(geometry, material);
+      self.scene.add(self.cubes);
+    };
+
+    createCubes();
+
+    console.log(self.cubes);
 
     self.camera.position.set(0,0,2000);
     self.controls.update();
@@ -106,7 +297,8 @@ function dlHomeController($scope,$timeout,$element) {
     self.camera.matrixWorldInverse.getInverse(self.camera.matrixWorld);
 
     // Update Mesh
-
+    self.cubes.material.uniforms.time.value += 0.06;
+    self.cubes.material.uniforms.time.value %= 30;
 
     // Update Control
     self.controls.update();
